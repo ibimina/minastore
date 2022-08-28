8da62d243da8c3bcea8df6eae20fad4517e92d93
ibimina <96787210+ibimina@users.noreply.github.com>
2022-08-28 20:41:34 +0000

    added routes to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 import './App.css';
+import NavBar from './components/NavBar';
+import Description from './pages/Description';
+import Home from './pages/home/Home';
+import ProductsNav from './pages/products/ProductsNav';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductsNav />} />
+          <Route path="/products/:id" element={<Description/>} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
