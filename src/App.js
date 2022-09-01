8893660c346873bfe5aeb5da45180fa735c550a3
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-01 19:18:31 -0400

    add routes to projects

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 import './App.css';
 import NavBar from './components/NavBar';
-import Description from './pages/Description';
+import Footer from "./components/Footer";
+import About from './pages/About';
+import Description from './pages/description/Description';
 import Home from './pages/home/Home';
-import ProductsNav from './pages/products/ProductsNav';
+import Blush from './pages/products/Blush';
+import Bronzer from './pages/products/Bronzer';
+import EyeLiner from './pages/products/EyeLiner';
+import EyeShadow from './pages/products/EyeShadow';
+import EyeBrow from "./pages/products/EyeBrow";
+import Mascara from './pages/products/Mascara';
+// import ProductsNav from './pages/products/ProductsNav';
+import Foundation from './pages/products/Foundation';
+import LipStick from "./pages/products/LipStick";
+import LipLiner from "./pages/products/LipLiner";
+import NailPolish from"./pages/products/NailPolish"
+import Cart from './pages/Cart';
+
 
 function App() {
   return (
@@ -13,10 +27,23 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductsNav />} />
-          <Route path="/products/:id" element={<Description/>} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/products" element={<ProductsNav />} /> */}
+          <Route path="/products/blush" element={<Blush />} />
+          <Route path="/products/bronzer" element={<Bronzer />} />
+          <Route path="/products/eyeshadow" element={<EyeShadow />} />
+          <Route path="/products/eyeliner" element={<EyeLiner />} />
+          <Route path="/products/mascara" element={<Mascara />} />
+          <Route path="/products/eyebrow" element={<EyeBrow />} />
+          <Route path="/products/foundation" element={<Foundation />} />
+          <Route path="/products/lipliner" element={<LipLiner />} />
+          <Route path="/products/lipstick" element={<LipStick />} />
+          <Route path="/products/nailpolish" element={<NailPolish />} />
+          <Route path="/products/:id" element={<Description />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
