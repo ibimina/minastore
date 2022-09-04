ac3ce053b5a9bdf84c20e48f90a966e9a0019e63
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-04 21:37:39 +0100

    refactored code

--- src/pages/products/ProductsNav.js
+++ src/pages/products/ProductsNav.js
@@ -1,62 +1,33 @@
 
-import ProductNavList from "../../components/ProductNavList";
+
+import Category from "../../components/Category";
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 import { useFetch } from "../../hooks/useFetch";
 
 import "./product.css"
 
-export default function ProductsNav() {
+export default function ProductsNav({visi,handleVisi,handle}) {
+ 
   const path = "./data.json";
   const { documents, isPending, error } = useFetch(path);
 
-  const handleClick = ( e) => {
 
-    let arrow = e.target.parentNode;
-    if (arrow.className.includes("arrow")) {
-   
-      let subColl = arrow.parentNode.nextSibling;
-      let visi = arrow.parentNode.nextSibling.getAttribute("data-visible");
-    
-      if (visi === "false") {
-        subColl.setAttribute("data-visible", true);
-        arrow.setAttribute("aria-pressed", true);
-      } else {
-        subColl.setAttribute("data-visible", false);
-        arrow.setAttribute("aria-pressed", false);
-      }
-
-    }
-
-  };
   return (
-    <>
+    <div className="pro-list" data-visible={visi}>
+  
+      <button onClick={handleVisi}>
+        <span className="sr-only">back</span>
+      </button>
       <h2 className="prod">Products</h2>
-      {error && <>error...</>}
-      {isPending && <>Loading...</>}
+      {error && <Error/>}
+      {isPending && <Loading/>}
       <ul className="collections">
         {documents &&
           documents.products.map((doc) => (
-            <li key={doc.bodyproduct} className="list">
-              <div className="container">
-                <p> {doc.bodyproduct}</p>
-                <figure
-                  className="arrow"
-                  onClick={(e) => handleClick(e)}
-                  aria-pressed="false"
-                >
-                  <img src={doc.image} alt="arrow down icon" />
-                </figure>
-              </div>
-
-              <ul className='part' data-visible="false">
-                {doc.subproduct.map((sub) => (
-                  <li key={sub.name}>
-                    <ProductNavList imageSrc={sub.images} caption={sub.name} />
-                  </li>
-                ))}
-              </ul>
-            </li>
+            <Category key={doc.bodyproduct} doc={doc} hand={handleVisi} handle={handle}/>
           ))}
       </ul>
-    </>
+    </div>
   );
 }
