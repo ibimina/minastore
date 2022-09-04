9112202df15ef25d39c060e4ba2590c6774fba51
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-04 21:36:51 +0100

    added a function that returns documents to next

--- src/pages/home/ProductList.js
+++ src/pages/home/ProductList.js
@@ -4,24 +4,33 @@ import ProductCon from "../../components/ProductCon";
 
 
 export default function ProductList({documents}) {
+const fetchData=()=>{
+ return documents
+}
+
   return (
     <section className="product-list">
-      <h1>product list</h1>
+      <h2 className="top-text">Product list</h2>
       <InfiniteScroll
         dataLength={documents.length}
-        next={documents}
+        next={fetchData}
         hasMore={true}
-        loader={<h4>Loading...</h4>}
+        loader={<h4> Yay! You have seen it all</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>Yay! You have seen it all</p>
+        }
       >
         <div className="product-listcon">
           {documents.map((doc, index) => (
-            <ProductCon key={index}
+            <ProductCon
+              key={index}
               imageSrc={doc.api_featured_image}
               productname={doc.name}
               productamount={doc.price}
               ratings={doc.rating}
               productid={doc.id}
               category={doc.category}
+              tag={doc.tag_list.join(",")}
             />
           ))}
         </div>
