9a122583244f735b2ff02897b39b6d99238f2ae4
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-04 21:30:52 +0100

    about component

---
+++ src/pages/About.js
@@ -0,0 +1,111 @@
+export default function About() {
+  return (
+    <div>
+      <h1>minastore</h1>
+
+      <section className="faqs section" id="faqs">
+        <p
+          className=""
+          data-sr-id="13"
+          >
+          SECURELY BUY AND TRACK
+        </p>
+        <h2
+          className=""
+          data-sr-id="14"
+          >
+          Frequenly Asked <span className="">Question</span>
+        </h2>
+        <p
+          className=""
+          data-sr-id="15"
+             >
+          We know you have some questions in mind, we have tried to list the
+          most important ones.
+        </p>
+
+        <div
+          className=""
+          data-sr-id="17"
+          >
+          <div className="">
+            <div className="">
+              {/* <header className=""> */}
+                <h3 className="">
+                  What form of payment do you accept?
+                </h3>
+          
+              {/* </header> */}
+
+              <div className="">
+                <p className="">
+                  We accept installment payments on all credit cards. 
+                </p>
+              </div>
+            </div>
+
+            <div className="">
+              {/* <header> */}
+                <h3 className="">How can I get in touch?</h3>
+            
+              {/* </header> */}
+
+              <div className="">
+                <p className="">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facere corrupti repudiandae quaerat error neque provident,
+                  consequatur in fuga itaque omnis, velit hic corporis quis vero
+                  consectetur architecto unde magnam officiis!
+                </p>
+              </div>
+            </div>
+
+            <div className="">
+              {/* <header className=""> */}
+                <h3 className="">Do you have any discounts?</h3>
+               
+              {/* </header> */}
+
+              <div className="">
+                <p className="">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Repellat aliquid ipsam officia! Earum aperiam tempore facere
+                  vitae exercitationem enim mollitia, sit velit. Fuga repellat
+                  doloremque id, aliquam quaerat itaque maxime!
+                </p>
+              </div>
+            </div>
+            <div className="">
+              {/* <header className=""> */}
+                <h3 className="">What guarantee do I have?</h3>
+            
+              {/* </header> */}
+
+              <div className="">
+                <p className="">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Ipsum iste accusantium adipisci sint quas nulla ad dolores
+                  dolorum dicta temporibus, magnam deleniti at distinctio
+                  doloremque sed sit corporis laboriosam. Dolores.
+                </p>
+              </div>
+            </div>
+
+            <div className="">
+              {/* <header className=" "> */}
+                <h3 className="">Can I pay in installments?</h3>
+                <i className=""></i>
+              {/* </header> */}
+
+              <div className="">
+                <p className="">
+                  No, full payments have to be make to purchase any products
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
