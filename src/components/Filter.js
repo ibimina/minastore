98b7a929005e6376eb9838d82ea2551fe2b14f40
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-07 01:50:44 +0100

    added div container

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,7 +2,7 @@
 import { useFetch } from "../hooks/useFetch";
 
 import { useState } from "react";
-export default function Filter({type,handleChange}) {
+export default function Filter({ type, handleChange }) {
   const [filter, setFilter] = useState("false");
   const urll =
     "https://makeup-api.herokuapp.com/api/v1/products.json?product_type=" +
@@ -11,32 +11,35 @@ export default function Filter({type,handleChange}) {
   let r = [];
   if (documents) {
     documents.forEach((element) => {
-    
       if (!r.includes(element.brand)) {
         r.push(element.brand);
       }
     });
   }
-    const handleClick = (e) => {
-      let rr = e.target.getAttribute("aria-pressed");
-      if (rr === "false") {
-        e.target.setAttribute("aria-pressed", "true");
-        setFilter("true");
-      } else {
-        e.target.setAttribute("aria-pressed", "false");
-        setFilter("false");
-      }
-    };
+  const handleClick = (e) => {
+    let rr = e.target.getAttribute("aria-pressed");
+    if (rr === "false") {
+      e.target.setAttribute("aria-pressed", "true");
+      setFilter("true");
+    } else {
+      e.target.setAttribute("aria-pressed", "false");
+      setFilter("false");
+    }
+  };
   return (
     <>
-      <p>Brands </p>
-      <button onClick={(e) => handleClick(e)} aria-pressed="false">
-        <span className="sr-only">menu</span>
-      </button>
+      <div className="flex mar">
+        <p>Brands </p>
+        <button onClick={(e) => handleClick(e)} aria-pressed="false" className="ops-btn ft-arrow">
+          <span className="sr-only">menu</span>
+        </button>
+      </div>
+
       <form
         onSubmit={(e) => handleChange(e)}
         data-visible={filter}
-        className="fibrands" >
+        className="fibrands"
+      >
         <div className="brandss">
           {documents &&
             r.map((bran) => (
