ff5ea4861db7dd9bb85d483f13804fa716d6f7b0
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-07 01:46:27 +0100

    refactored code

--- src/pages/description/Description.js
+++ src/pages/description/Description.js
@@ -9,47 +9,25 @@ import { useFetch } from "../../hooks/useFetch";
 
 // page components
 import Goback from "../../components/Goback";
-import GetColors from "../../components/GetColors";
-import Image from "../../components/Image";
 import Error from "../../components/Error";
 import Loading from "../../components/Loading";
 import Extras from "../../components/Extras";
+import Describe from "../../components/Describe";
 
 export default function Description() {
   const { id } = useParams();
-  const query = "https://makeup-api.herokuapp.com/api/v1/products/" + id + ".json";
-
+  const query =
+    "https://makeup-api.herokuapp.com/api/v1/products/" + id + ".json";
   const { documents, isPending, error } = useFetch(query);
- 
   return (
     <>
       <div className="desc-con">
         <Goback />
         {error && <Error />}
         {isPending && <Loading />}
-
         {documents && (
           <>
-            <h1>
-              <span>{documents.brand}</span> {documents.product_type}
-            </h1>
-            <div className="des-d">
-              <Image
-                imageSrc={documents.api_featured_image}
-                alt={documents.name}
-                className="descimg"
-              />
-              <div>
-                <GetColors documents={documents} id={id} />
-              </div>
-            </div>
-            <p>
-              tags:{" "}
-              {documents.tag_list.join(",")
-                ? documents.tag_list.join(",")
-                : "no tag avaliable"}
-            </p>
-            <p>Description: {documents.description}</p>
+            <Describe documents={documents} id={id} />
             <Extras producttype={documents.product_type} />
           </>
         )}
