f8d5df789f7a846c91baba6b606dcffe6ca49e4e
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-07 01:47:04 +0100

    added new usecontext states

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,19 +3,47 @@ import { createContext } from "react";
 
 export const CartContext = createContext();
 
-export const cartReducer = (action, state) => {
+export const cartReducer = ( state,action) => {
   switch (action.type) {
     case "ADD_ITEM":
       return { ...state, quantity: action.payload };
+    case "ADD_NAME":
+      return { ...state,  firstNames: action.payload  };
+    case "ADD_LNAME":
+      return { ...state, lastNames: action.payload};
+    case "ADD_SHIPP": return {  ...state, shippingPrices: action.payload };
+    case "ADD_COUNTRY":
+      return { ...state,  countrys: action.payload };
+    case "ADD_CITY":
+      return {  ...state,  citys: action.payload};
+    case "ADD_ADDRESS":
+      return { ...state, addresss: action.payload };
+    case "ADD_EMAIL":
+      return { ...state, emails: action.payload   };
+    case "ADD_POSTALCODE":
+return {  ...state,  postalCodes: action.payload    };
+    case "ADD_PHONENUMBER":
+      return {
+        ...state,  phonenumbers: action.payload };
     default:
       return state;
   }
 };
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, {
     quantity: null,
+    shippingPrices: 0,
+    firstNames: null,
+    lastNames: null,
+    citys: "",
+    countrys: "",
+    emails: "",
+    addresss: "",
+    postalCodes: "",
+    phonenumbers: ""
   });
+
   return (
     <CartContext.Provider value={{ ...state, dispatch }}>
       {children}
