7c666192232121014e4b72cf8bc7e3873176c9ea
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-08 01:02:06 +0100

    changed image type

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,7 +5,7 @@ export default function Error() {
     <>
       <p className="error-msg">Ooops server down</p>
       <Image
-        imageSrc={process.env.PUBLIC_URL + "/images/error.png"}
+        imageSrc={process.env.PUBLIC_URL + "/images/error.webp"}
         alt="error icon"
         className="error"
       />
