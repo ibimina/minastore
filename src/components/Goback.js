5e476c166ed918d652f8e63e783e7b4745165e23
ibimina <96787210+ibimina@users.noreply.github.com>
2022-09-08 01:09:15 +0100

    change navigation from home to -1

--- src/components/Goback.js
+++ src/components/Goback.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 export default function Goback() {
   const navigate = useNavigate();
   return (
-    <button onClick={() => navigate("/")} className="back ops-btn">
+    <button onClick={() => navigate("-1")} className="back ops-btn">
       <span className="sr-only">back</span>
     </button>
   );
